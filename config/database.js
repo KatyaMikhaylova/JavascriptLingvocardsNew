b451deb04dae672e39cf7830db23db9b0d72216e
redmine <ekyastrebova@mail.ru>
2018-08-17 14:37:48 +0300

    modified mlab connection

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
-  database:'mongodb://admin:WSXedc45@ds056559.mlab.com:56559/jslcdb',
+  database: process.env.MONGODB_URI ||'mongodb://admin:WSXedc45@ds056559.mlab.com:56559/jslcdb',
   secret: 'yoursecret'
 }
 
