a487b5828ddf923c1467f4c1cca3eab5e1a639ef
redmine <ekyastrebova@mail.ru>
2018-08-20 10:53:08 +0300

    removed fetch

--- public/js/main.js
+++ public/js/main.js
@@ -35,16 +35,44 @@ $(document).ready(function () {
         $target = $(x.target)||$(x.event);
         const id = $target.attr('coll-id');
 
-        fetch('/lingvo/getdata/'+id, {
-            credentials: 'include'
-        })
-            .then(
-                //res => res.json()
-                function(response) {
-                    return response.json();
-                }
-            )
-            .then(Lingvo);
+        // fetch('/lingvo/getdata/'+id, {
+        //     credentials: 'include'
+        // })
+        //     .then(
+        //         res => res.json()
+        //         // function(response) {
+        //         //     return response.json();
+        //         }
+        //     )
+        //     .then(Lingvo);
+         if (window.XMLHttpRequest) { // Mozilla, Safari, ...
+             request = new XMLHttpRequest();
+         } else if (window.ActiveXObject) { // IE
+             try {
+                 request = new ActiveXObject('Msxml2.XMLHTTP');
+             }
+             catch (e) {
+                 try {
+                     request = new ActiveXObject('Microsoft.XMLHTTP');
+                 }
+                 catch (e) {}
+             }
+         }
+
+         function onLoadListener() {
+             var data = JSON.parse(this.responseText);
+
+             Lingvo(data);
+         }
+
+         function onErrorListener(err) {
+             console.log('XHR Error :', err);
+         }
+
+         request.onload = onLoadListener;
+         request.onerror = onErrorListener;
+         request.open('get', '/lingvo/getdata/'+id, true);
+         request.send();
     });
 
     function Lingvo(data) {
